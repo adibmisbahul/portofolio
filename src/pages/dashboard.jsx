5260cd33751f15ce50c68cbcbccd52a6f2229987
adibmisbahul <adibmisbahul101812@gmail.com>
2026-01-28 10:17:11 +0700

    remake layout

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -15,7 +15,9 @@ export default function Dashboard() {
   ];
   return (
     <div>
-      <div className="bg-zinc-950 flex-col items-center justify-center">
+      <div className="bg-zinc-950 flex flex-col items-center justify-center h-dvh relative z-10">
+        <div class="absolute -z-10 size-100 brightness-20 bg-violet-700 top-0 rounded-r-full blur-[100px]"></div>
+        <div class="absolute -z-10 size-100 brightness-20 bg-sky-700 bottom-0 right-0 rounded-l-full blur-[100px]"></div>
         <div className="flex gap-1 p-3 justify-center">
           {firstTag.map((item) => {
             return (
@@ -23,6 +25,17 @@ export default function Dashboard() {
             );
           })}
         </div>
+        <h1 className="text-5xl font-semibold md:text-8xl md:font-semibold">
+          Crafting Digital
+        </h1>
+        <h1 className="font-semibold text-5xl m-1 text-transparent bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text xl:text-8xl">
+          Experiences
+        </h1>
+        <p className="w-2/3 text-sm mt-10 font-semibold text-zinc-400 md:text-xl">
+          A multi-disciplinary engineer crafting robust software, intelligent
+          IoT systems, and reliable network infrastructure with a passion for
+          innovation.
+        </p>
       </div>
     </div>
   );
